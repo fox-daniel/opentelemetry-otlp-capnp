86b1e5661150c15932e9fe91422fafd49835665c
Daniel Fox <foxdanie@gmail.com>
2025-11-20 14:42:12 -0500

    refactor into app and telemetry receiver

--- opentelemetry-otlp-capnp/examples/subscriber/src/main.rs
+++ opentelemetry-otlp-capnp/examples/subscriber/src/main.rs
@@ -5,9 +5,18 @@ use tracing_subscriber::fmt::format::FmtSpan;
 const TEST_ADDRESS: &str = "127.0.0.1:8080";
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    run_telemetry_receiver().await?;
+    run_app().await?;
+    Ok(())
+}
+
+async fn run_telemetry_receiver() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
+
+async fn run_app() -> Result<(), Box<dyn std::error::Error>> {
     // let subscriber = CapnpSubscriber;
-    // let (non_blocking, _guard) = tracing_appender::non_blocking(stdout());
     let subscriber = tracing_subscriber::fmt()
         .with_span_events(FmtSpan::FULL)
         .finish();
