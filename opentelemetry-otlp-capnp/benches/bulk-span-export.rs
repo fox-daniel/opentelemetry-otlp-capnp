881b39f2e1fc236aae79d111a4202c72bf079a96
Daniel Fox <foxdanie@gmail.com>
2025-12-17 14:44:04 -0500

    add utilities for benchmarks

--- opentelemetry-otlp-capnp/benches/bulk-span-export.rs
+++ opentelemetry-otlp-capnp/benches/bulk-span-export.rs
@@ -1,17 +1,32 @@
 use criterion::Throughput;
 use criterion::{criterion_group, criterion_main};
 use criterion::{BatchSize, Criterion};
+use utilities::capnp::FakeCapnp;
 
 #[derive(Clone)]
-struct TestInput {}
+struct TestInput {
+    rb: u32,
+}
+
+impl TestInput {
+    fn new() -> Self {
+        TestInput { rb: 0 }
+    }
+
+    fn export(&self) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(())
+    }
+}
 
 fn export_spans(c: &mut Criterion) {
-    let req_small = FakeCapnp::trace_service_request();
+    let req_small = FakeCapnp::trace_service_request_with_spans(1, 1);
     let input = [("small", req_small)];
     let mut group = c.benchmark_group("export spans");
     for size_and_req in input.iter() {
         group.throughput(Throughput::Bytes(size_and_req.1.encoded_len() as u64));
-        group.bench_function();
+        group.bench_function(format!("export spans {}", size_and_req.0), move |b| {
+            b.iter_batched(|| TestInput::new(), |ti| ti.export(), BatchSize::SmallInput)
+        });
     }
     group.finish();
 }
---
+++ opentelemetry-otlp-capnp/utilities/src/capnp.rs
@@ -0,0 +1,5 @@
+pub struct FakeCapnp;
+
+impl FakeCapnp {
+    pub fn trace_service_request_with_spans(num_resource_spans: usize, num_spans: usize) {}
+}
