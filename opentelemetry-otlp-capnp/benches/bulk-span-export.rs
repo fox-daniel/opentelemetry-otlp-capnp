76aca807f619e523340f702665c0ce4c8be7151d
Daniel Fox <foxdanie@gmail.com>
2025-12-18 10:14:19 -0500

    fix name to not have spaces

--- opentelemetry-otlp-capnp/benches/bulk-span-export.rs
+++ opentelemetry-otlp-capnp/benches/bulk-span-export.rs
@@ -34,7 +34,7 @@ fn export_spans(c: &mut Criterion) {
     let input = [("small", req_small)];
     let mut group = c.benchmark_group("export spans");
     for (name, req) in input.into_iter() {
-        group.bench_function(format!("export spans {}", name), |b| {
+        group.bench_function(format!("export_spans_{}", name), |b| {
             let exporter = SpanExporter::builder()
                 .with_capnp()
                 .with_endpoint(ENDPOINT)
