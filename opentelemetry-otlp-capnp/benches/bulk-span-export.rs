dd1127debfce193baa22ca0ec5e0ee56f8430729
Daniel Fox <foxdanie@gmail.com>
2025-12-18 11:36:23 -0500

    wip benchmark comparing otlp and capnp

--- opentelemetry-otlp-capnp/benches/bulk-span-export.rs
+++ opentelemetry-otlp-capnp/benches/bulk-span-export.rs
@@ -1,12 +1,14 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId};
 use criterion::{BatchSize, Criterion};
 use opentelemetry_capnp::transform::trace::SpanRequest;
-use opentelemetry_otlp_capnp::{SpanExporter, SpanReceiver, WithExportConfig};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_otlp_capnp::{SpanExporter, SpanReceiver, WithExportConfig as _};
 use opentelemetry_sdk::trace::SpanExporter as _;
 use tokio::runtime::Runtime;
 use utilities::capnp::FakeCapnp;
 
-const ENDPOINT: &str = "127.0.0.1:4317";
+const CAPNP_ENDPOINT: &str = "127.0.0.1:4318";
+const OTLP_ENDPOINT: &str = "127.0.0.1:4317";
 
 #[derive(Clone)]
 struct TestInput {
@@ -26,7 +28,7 @@ impl TestInput {
 
 fn export_spans(c: &mut Criterion) {
     let rt = Runtime::new().expect("able to create new runtime");
-    let _span_receiver = SpanReceiver::new(ENDPOINT)
+    let _span_receiver = SpanReceiver::new(CAPNP_ENDPOINT)
         .start()
         .map_err(|e| format!("Failed to start SpanReceiver: {e}"));
     std::thread::sleep(std::time::Duration::from_millis(100));
@@ -37,7 +39,7 @@ fn export_spans(c: &mut Criterion) {
         group.bench_function(BenchmarkId::new("export_spans", name), |b| {
             let exporter = SpanExporter::builder()
                 .with_capnp()
-                .with_endpoint(ENDPOINT)
+                .with_endpoint(CAPNP_ENDPOINT)
                 .build()
                 .expect("build SpanExporter with endpoint: {ENDPOINT}");
             b.iter_batched(
@@ -50,5 +52,54 @@ fn export_spans(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, export_spans);
+fn span_export_comparison(c: &mut Criterion) {
+    let rt = Runtime::new().expect("able to create new runtime");
+    let _capnp_span_receiver = SpanReceiver::new(CAPNP_ENDPOINT)
+        .start()
+        .map_err(|e| format!("Failed to start SpanReceiver: {e}"));
+    let _otlp_span_receiver = SpanReceiver::new(OTLP_ENDPOINT)
+        .start()
+        .map_err(|e| format!("Failed to start SpanReceiver: {e}"));
+    std::thread::sleep(std::time::Duration::from_millis(100));
+    let req_small = FakeCapnp::trace_service_request_with_spans(1);
+    let req_medium = FakeCapnp::trace_service_request_with_spans(10);
+    let req_large = FakeCapnp::trace_service_request_with_spans(100);
+    let req_huge = FakeCapnp::trace_service_request_with_spans(1000);
+    let input: [(&str, SpanRequest); 4] = [
+        ("small", req_small),
+        ("medium", req_medium),
+        ("large", req_large),
+        ("huge", req_huge),
+    ];
+    let mut group = c.benchmark_group("span export comparison");
+    let capnp_exporter = SpanExporter::builder()
+        .with_capnp()
+        .with_endpoint(CAPNP_ENDPOINT)
+        .build()
+        .expect("build Capnp SpanExporter with endpoint: {ENDPOINT}");
+    for (name, req) in input.iter() {
+        group.bench_with_input(
+            BenchmarkId::new("Cap'n Proto span export", name),
+            req,
+            |b, req| {
+                b.iter(|| rt.block_on(async { capnp_exporter.export(req.batch.clone()).await }))
+            },
+        );
+    }
+    let otlp_exporter = rt.block_on(async {
+        opentelemetry_otlp::SpanExporter::builder()
+            .with_tonic()
+            .with_endpoint(OTLP_ENDPOINT)
+            .build()
+            .expect("build OTLP SpanExporter with endpoint: {ENDPOINT}")
+    });
+    for (name, req) in input.iter() {
+        group.bench_with_input(BenchmarkId::new("OTLP span export", name), req, |b, req| {
+            b.iter(|| rt.block_on(async { otlp_exporter.export(req.batch.clone()).await }))
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, span_export_comparison);
 criterion_main!(benches);
