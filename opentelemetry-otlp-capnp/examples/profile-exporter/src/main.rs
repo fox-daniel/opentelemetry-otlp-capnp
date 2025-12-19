5a9e14a4da894058d59040cb20727d4d604bc72e
Daniel Fox <foxdanie@gmail.com>
2025-12-19 13:59:56 -0500

    use noop receiver in profiling

--- opentelemetry-otlp-capnp/examples/profile-exporter/src/main.rs
+++ opentelemetry-otlp-capnp/examples/profile-exporter/src/main.rs
@@ -6,11 +6,11 @@ const CAPNP_ENDPOINT: &str = "127.0.0.1:4318";
 const NUM_ITERS: usize = 100;
 
 async fn span_export() -> Result<(), Box<dyn std::error::Error>> {
-    let _capnp_span_receiver = SpanReceiver::new(CAPNP_ENDPOINT)
+    let _capnp_span_receiver = NoOpSpanReceiver::new(CAPNP_ENDPOINT)
         .start()
         .map_err(|e| format!("Failed to start SpanReceiver: {e}"));
     std::thread::sleep(std::time::Duration::from_millis(100));
-    let req = FakeCapnp::trace_service_request_with_spans(10);
+    let req = FakeCapnp::trace_service_request_with_spans(100);
     let capnp_exporter = SpanExporter::builder()
         .with_capnp()
         .with_endpoint(CAPNP_ENDPOINT)
